4a2b7cf10067f091e104e30ac05c9c14729cfa86
Naiara Martins <naiaraxf@gmail.com>
2024-09-05 21:16:03 -0300

    feat:sum of payments for properties

--- src/controller/data.controller.js
+++ src/controller/data.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const { PaymentRepository } = require('../model/data.model');
 
 class PaymentController {
@@ -13,6 +14,50 @@ class PaymentController {
       console.error(err);
       res.status(500).json({ message: err.sqlMessage });
     }
+  }
+
+  // Soma dos pagamentos por propriedades
+  async getSumsPaymentsByProperty(_req, res) {
+    try {
+      // Obtém os detalhes dos pagamentos
+      const payments = await this.paymentRepository.getPaymentDetailsByProperty();
+
+      // Reduz os pagamentos para calcular as somas e obter as descrições das propriedades
+      const propertySums = PaymentController.calculatePropertySums(payments);
+
+      // Transforma o resultado em um array com o formato desejado
+      const result = Object.entries(propertySums).map(([code_property, { property_description, total_payment }]) => ({
+        code_property,
+        property_description,
+        total_payment: total_payment.toFixed(2),
+      }));
+
+      // Retorna o resultado como JSON
+      res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  // Faz a soma dos pagamentos, agrupando pelo código da propriedade
+  static calculatePropertySums(payments) {
+    return payments.reduce((acc, { code_property, payment_amount, property_description }) => {
+      if (!acc[code_property]) {
+        acc[code_property] = { property_description, total_payment: 0 };
+      }
+      acc[code_property].total_payment += parseFloat(payment_amount);
+      return acc;
+    }, {});
+  }
+
+  static calculateMonthlyTotals(payments) {
+    return payments.reduce((acc, payment) => {
+      const date = new Date(payment.payment_date_rental);
+      const monthYear = `${date.getMonth() + 1}/${date.getFullYear()}`;
+      acc[monthYear] = (acc[monthYear] || 0) + parseFloat(payment.payment_amount);
+      return acc;
+    }, {});
   }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
-const { PaymentController } = require('../controller/data.controller');
 const express = require('express');
+const { PaymentController } = require('../controller/data.controller');
 
 const router = express.Router();
 const paymentController = new PaymentController();
 
-router.get('/', (_req, res) =>
-  paymentController.getAllPaymentDetailsByProperty(_req, res),
-);
+router.get('/', (_req, res) => paymentController.getAllPaymentDetailsByProperty(_req, res));
+router.get('/api/payments-summary', (req, res) => paymentController.getSumsPaymentsByProperty(req, res));
 
 module.exports = router;
